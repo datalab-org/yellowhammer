7b44707a33bd66e2fc2f18d7d803960d77cac679
Yue Wu <14996988+yue-here@users.noreply.github.com>
2024-10-22 12:26:43 -0700

    inital commit for yellowhammer jupyter assistant

--- src/yellowhammer/__init__.py
+++ src/yellowhammer/__init__.py
@@ -6,3 +6,14 @@ except PackageNotFoundError:
     __version__ = "develop"
 
 __all__ = ("__version__",)
+
+
+from .magics import datalabMagics
+
+def load_ipython_extension(ipython):
+    """
+    Any module file that define a function named `load_ipython_extension`
+    can be loaded via `%load_ext module.path` or be configured to be
+    autoloaded by IPython at startup time.
+    """
+    ipython.register_magics(datalabMagics)
